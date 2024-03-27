b88959b9c68d5177c6c9d86d94450f676a6979a3
원현섭 <123632194+Wonseoop@users.noreply.github.com>
2024-03-27 17:54:05 +0900

    수정

--- model/Pose_Estination/Mediapipe/webcam.py
+++ model/Pose_Estination/Mediapipe/webcam.py
@@ -17,3 +17,28 @@ while webcam.isOpened():
 
 webcam.release()
 cv2.destroyAllWindows()
+
+
+# 모듈 불러오기: import 모듈 이름 - 이미지 사용을 위한 opencv,
+# 이미지 저장 파일명 사용을 위한 datetime
+
+#qimport cv2
+#import datetime
+
+#video_capture = cv2.VideoCapture(0)
+
+#while (True):
+
+#    grabbed, frame = video_capture.read()
+#    cv2.imshow('Original Video', frame)
+
+ #   key = cv2.waitKey(1);
+ #   if key == ord('q'):
+ #       break
+ #   elif key == ord('s'):
+ #       file = datetime.datetime.now().strftime("%Y%m%d_%H%M%S%f") + '.jpg'
+ #       cv2.imwrite(file, frame)
+ #       print(file, ' saved')
+
+#video_capture.release()
+#cv2.destroyAllWindows()
--- model/openpose/openpose2/path.py
+++
@@ -1,1 +0,0 @@
-print('openpose2\\human-pose-estimation-opencv-master\\graph_opt.pb')
