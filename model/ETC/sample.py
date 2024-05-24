7702dacf49a9db99c52791be91322561668d1f68
원현섭 <dongjin778@naver.com>
2024-05-24 23:07:38 +0900

    d

--- model/ETC/sample.py
+++ model/ETC/sample.py
@@ -1,10 +1,14 @@
 import random, time, os
-print("CheckPoint 1",flush=True)
+print("CheckPoint 1 : Start feedback",flush=True)
 time.sleep(1)
-print("CheckPoint 2",flush=True)
+print("CheckPoint 2 : Successfully split into 60 frames", flush=True)
+
 time.sleep(1)
-print("CheckPoint 3",flush=True)
+print("CheckPoint 3 : Sequence Data Extracted Successfully", flush=True)
+
 time.sleep(1)
+print("CheckPoint 4 : prediction success", flush=True)
+
 
 file_path = os.path.abspath(__file__)
 dir = os.path.dirname(file_path)
@@ -13,13 +17,13 @@ dir = os.path.dirname(file_path)
 gif_output_folder = os.path.abspath(os.path.join(dir, "..", "..","server","FBHtml","gif"))
 
 #경보가 만들어줄꺼임
-gif_path=os.path.join(gif_output_folder, "sample.gif")
+gif_path=os.path.join(gif_output_folder, "output.gif")
 
 
 print(f"gif_path:{gif_path}",flush=True)
 
 
-true_percentage = random.randint(1,100)
+true_percentage = 80#random.randint(1,100)
 accuracy=true_percentage
 # Returning messages based on conditions
 if true_percentage >= 95:
