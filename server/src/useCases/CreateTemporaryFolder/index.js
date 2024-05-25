78f1fea9da82c39c8401f448dee30503422517cc
Your Name <you@example.com>
2024-05-25 18:41:31 +0900

    d

--- server/src/useCases/CreateTemporaryFolder/index.js
+++
@@ -1,6 +0,0 @@
-const { resolve } = require("path");
-const { CreateTemporaryFolder } = require("./CreateTemporaryFolder");
-
-const createTemporaryFolder = new CreateTemporaryFolder(resolve(__dirname, '..', '..', '..', 'temp'));
-
-module.exports = { createTemporaryFolder };
